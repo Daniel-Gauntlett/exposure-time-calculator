30f898427b70245e6980c19a9e5ac3922427324e
Daniel-Gauntlett <dangauntlett@gmail.com>
2025-11-13 21:59:18 -0500

    add calculator

---
+++ calculator.py
@@ -0,0 +1,124 @@
+import mathfunctions
+import math
+pi = 3.1415926535897932384626
+e = 2.718281828459
+
+def normalize(star,vega,star_mag):
+    star_input = []
+    starx = []
+    stary = []
+    starpairs = []
+    with open(star, 'r') as curve:
+        for line in curve:
+            if line[0] == ' ' and len(line) > 2 and line[1] != '"' and line[2] != '-':
+                if line[-1] == '\n':
+                    end = -1
+                else:
+                    end = len(line)
+                star_input.append(list(filter(lambda num: num != "", line[0:end].split(" "))))
+                starx.append(float(starpairs[-1][0]))
+                stary.append(float(starpairs[-1][1]) / 100)
+                starpairs.append((starx[-1],stary[-1]))
+    vega_input = []
+    vegax = []
+    vegay = []
+    vegapairs = []
+    with open(vega, 'r') as curve:
+        for line in curve:
+            if line[0] == ' ' and len(line) > 2 and line[1] != '"' and line[2] != '-':
+                if line[-1] == '\n':
+                    end = -1
+                else:
+                    end = len(line)
+                vega_input.append(list(filter(lambda num: num != "", line[0:end].split(" "))))
+                vegax.append(float(vegapairs[-1][0]))
+                vegay.append(float(vegapairs[-1][1]) / 100)
+                vegapairs.append((vegax[-1],vegay[-1]))
+    vegainterped = mathfunctions.interpolate(vegapairs, [pair[0] for pair in starpairs])
+    scaled = [(i[0], 10 ** ((vegainterped - star_mag) / -2.5)) for i in starpairs]
+    return scaled
+    
+
+def filter(starpairs,filter):
+    # read in sed filter
+    sed_input = []
+    sedx = []
+    sedy = []
+    sed_pairs = []
+    with open(filter, 'r') as sed:
+        for line in sed:
+            if line[0] != '#':
+                if line[-1] == '\n':
+                    end = -1
+                else:
+                    end = len(line)
+                sed_input.append(list(filter(lambda num: num != "", line[0:end].split(" "))))
+                sedx.append(float(sed_input[-1][0]))
+                sedy.append(float(sed_input[-1][1]))
+                sed_pairs.append((sedx[-1],sedy[-1]))
+
+    # interpolate sed_pairs over v filter for proper multiplication
+
+    interpolated = mathfunctions.interpolate(sed_pairs, [pair[0] for pair in starpairs])
+
+    flux = mathfunctions.integrate([(interpolated[i][0], interpolated[i][1] * starpairs[i][1]) for i in range(len(starpairs))], starpairs[0][0], starpairs[-1][0])
+    return flux
+
+def extinction_correction(k, airmass):
+    return 10 ** (k * airmass / -2.5)
+
+def aperture_fraction(radius, FWHM):
+    return 1 - e ** (0 - (radius ** 2) / (2 * (FWHM/2.35) ** 2))
+
+def area(diameter):
+    return (pi * diameter / 4) ** 2
+
+def main():
+    config = open("config.txt")
+    settings = {}
+    for line in config:
+        if line[0] != "#" and len(line) > 1:
+            templine = line.split("=")
+            try:
+                settings[templine[0]] = eval(templine[1])
+            except:
+                settings[templine[0]] = templine[1]
+    # step 1: normalize template
+    template = normalize(settings["star_template"],settings["vega_template"],settings["target_mag"])
+    # step 2: filter
+    flux = filter(template,settings["filter"])
+    # step 3: extinction correction
+    ext_corr = extinction_correction(settings["k"],settings["airmass"])
+    # step 4: fraction in aperture
+    if settings["target_profile"] == "point":
+        fwhm = settings["seeing"]
+    elif settings["target_profile"] == "extend":
+        fwhm = settings["fwhm"]
+    else:
+        print("Error: invalid profile provided.")
+        return
+    aper_frac = aperture_fraction(settings["diameter"] / 2, fwhm)
+    # step 5: QE
+    qe = settings["QE"]
+    # step 6: telescope area
+    telescope_area = area(settings["diameter"])
+    nstar = flux * ext_corr * aper_frac * qe * telescope_area
+    nd = settings["dark_current"]
+    npix = pi * ((0.67 * fwhm / settings["pixel_scale"]) ** 2)
+    ns = settings["background_noise"]
+    nrd = settings["ccd"]
+    if settings["mode"] == "E":
+        mode = "exposure time"
+        s = settings["signal_noise_ratio"]
+        output = (s ** 2 * (npix * nd + npix * ns + nstar) * t + math.sqrt((s ** 2 * (npix * nd + npix * ns + nstar) * t) ** 2 - 4 * nstar ** 2 * s ** 2 * npix * nrd ** 2)) / (2 * nstar ** 2)
+    elif settings["mode"] == "S/N":
+        mode = "signal to noise ratio"
+        t = settings["exposure_time"]
+        output = nstar * t / (math.sqrt(nstar * t + npix(ns * t + nd * t + nrd ** 2)))
+    else:
+        print("Error: invalid mode provided.")
+        return
+    print("Your final " + mode + " is:" + output)
+
+main()
+
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Provide the settings in the config file, and run the program from the command line (or in any other way where it won't immediately close) to get an output.
+
+ALL units must match the ones in this document. The only exception is the files, since those are often harder to convert. Instead, you will provide the units in the config there.
